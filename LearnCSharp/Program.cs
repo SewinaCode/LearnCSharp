9605e57f3001c40e44270386baa945aad68dd038
SewinaCode <sellynalky@gmail.com>
2023-01-28 19:12:17 +1300

    Working with numbers comment line

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
+//Learning the fundamentals of C#
 namespace Giraffe1 // Note: actual namespace depends on the project name.
 {
     class Program
@@ -22,7 +22,10 @@ namespace Giraffe1 // Note: actual namespace depends on the project name.
 
             string phrase = "Giraffe Academy";
             //Substring (a, b) a is the first index of the character we want, b is the number of characters we want to take after a.
-            Console.WriteLine(phrase.Substring(phrase.IndexOf("Academy")));
+            Console.WriteLine(phrase.Substring(phrase.IndexOf("Academy"))); //There are C# string methods - google it. 
+
+            //Dealing with numbers - there are built in methods Math.[method]. Can google them C# methods. 
+            
 
             Console.ReadLine();
 
