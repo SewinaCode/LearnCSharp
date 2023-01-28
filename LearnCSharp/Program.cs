ab9dfaf2b0662d94622e8b111cd963db6816f9d9
SewinaCode <sellynalky@gmail.com>
2023-01-28 19:11:15 +1300

    Working with string and numbers fundamentals.

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -10,7 +10,7 @@ namespace Giraffe1 // Note: actual namespace depends on the project name.
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //Learning about variables and datatypes
             string characterName = "Tom";
             int characterAge;
             characterAge = 25;
@@ -19,6 +19,10 @@ namespace Giraffe1 // Note: actual namespace depends on the project name.
             Console.WriteLine("He was " + characterAge + " years old");
             Console.WriteLine("He really liked the name " + characterName);
             Console.WriteLine("But didn't like being " + characterAge);
+
+            string phrase = "Giraffe Academy";
+            //Substring (a, b) a is the first index of the character we want, b is the number of characters we want to take after a.
+            Console.WriteLine(phrase.Substring(phrase.IndexOf("Academy")));
 
             Console.ReadLine();
 
